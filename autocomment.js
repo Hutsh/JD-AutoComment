86c9de4ff1116c7f70e5e9d8364a363d59024895
Hutsh <hxbbun@gmail.com>
2016-09-28 18:15:55 +0800

    fix autocomment.js
    修复多个商品在同一界面评价错误

--- autocomment.js
+++ autocomment.js
@@ -1,17 +1,22 @@
-star5 = document.getElementsByClassName('star5');
-for(var i=0;i<star5.length;i++){star5[i].click();}
-mycomm="";
-conj=["而且",",","、","感觉"];
-comm_text = document.getElementsByClassName('tag-item');
-num=[];for(i=0;i<comm_text.length;i++){num[i]=i};
-num.sort(function(){return 0.5 - Math.random()});
-num2=[0,1,2,3];num2.sort(function(){return 0.5 - Math.random()});
-for(i=0;i<5;i++){
-	sn=num[i];sn2=num2[i];
-	comm_text[sn].click();
-	mycomm+=comm_text[sn].text;
-	if(i<4)mycomm+=conj[sn2];
+item=document.getElementsByClassName('f-item f-goods');
+for(j=0;j<item.length;j++){
+	console.log("第"+j+"个商品");
+	star5 = item[j].getElementsByClassName('star5');
+	for(var i=0;i<star5.length;i++){star5[i].click();}
+	mycomm="";
+	conj=["而且",",","、","感觉"];
+	comm_text = item[j].getElementsByClassName('tag-item');
+	num=[];for(i=0;i<comm_text.length;i++){num[i]=i};
+	num.sort(function(){return 0.5 - Math.random()});
+	num2=[0,1,2,3];num2.sort(function(){return 0.5 - Math.random()});
+	for(i=0;i<5;i++){
+		sn=num[i];sn2=num2[i];
+		comm_text[sn].click();
+		mycomm+=comm_text[sn].text;
+		if(i<4)mycomm+=conj[sn2];
+	}
+	mycomm+="。"
+	texta=item[j].getElementsByTagName('textarea');
+	texta[0].value=mycomm;
+
 }
-mycomm+="。"
-texta=document.getElementsByTagName('textarea');
-texta[0].value=mycomm;
