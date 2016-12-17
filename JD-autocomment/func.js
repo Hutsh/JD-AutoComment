c8e235e9c5899299db0f688a8578efd96d0de519
dengpeng <dengpeng@outlook.com>
2016-12-18 00:35:36 +0800

    auto run on comment page. refactor with jquery. add comment on no-tag page.

--- JD-autocomment/func.js
+++ JD-autocomment/func.js
@@ -1,29 +1,44 @@
-function addcomment(){
-	stars=document.getElementsByClassName('star star5');
-	for(var l=0;l<stars.length;l++){
-		stars[l].click();
-		//console.log("第"+l+"个");
-	}
-	item=document.getElementsByClassName('f-item f-goods');
-	for(var j=0;j<item.length;j++){
-		console.log("第"+j+"个商品");
-		//star5 = item[j].getElementsByClassName('star5');
-		//for(var i=0;i<star5.length;i++){star5[i].click();}
-		mycomm="";
-		conj=["而且",",","、","感觉"];
-		comm_text = item[j].getElementsByClassName('tag-item');
-		num=[];for(i=0;i<comm_text.length;i++){num[i]=i};
-		num.sort(function(){return 0.5 - Math.random()});
-		num2=[0,1,2,3];num2.sort(function(){return 0.5 - Math.random()});
-		for(i=0;i<5;i++){
-			sn=num[i];sn2=num2[i];
-			comm_text[sn].click();
-			mycomm+=comm_text[sn].text;
-			if(i<4)mycomm+=conj[sn2];
-		}
-		mycomm+="。";
-		texta=item[j].getElementsByTagName('textarea');
-		texta[0].value=mycomm;
-		console.log("第"+j+"个商品评价完成");
-	}
+$().ready(addComments);
+
+function addComments() {
+    $('.star.star5').click();
+
+    var items = $('.f-item.f-goods');
+    for (var i = 0; i < items.length; i++) {
+        console.log("第" + i + "个商品");
+        var my_comment = '';
+        var conjunctions = ["而且", ",", "、", "感觉"];
+        var tags = items.eq(i).find('.tag-item');
+        if (tags.length === 0) {
+            var title = items.eq(i).find('.p-name').text();
+            var title_arr = title.split(' ');
+            my_comment = '挺好的价格买到了' + title_arr[Math.floor(Math.random() * title_arr.length)] + ', 我觉得还是很满意的。';
+            items.eq(i).find('textarea').text(my_comment);
+            continue;
+        }
+        var num = [];
+        for (var j = 0; j <= tags.length; j++) {
+            num[j] = j;
+        }
+        num.sort(function () {
+            return 0.5 - Math.random()
+        });
+
+        var num2 = [0, 1, 2, 3];
+        num2.sort(function () {
+            return 0.5 - Math.random()
+        });
+        for (var k = 0; k < 5; k++) {
+            var sn = num[k];
+            var sn2 = num2[k];
+            tags.eq(sn).addClass('tag-checked');
+            my_comment += tags.eq(sn).text();
+            if (k < 4)
+                my_comment += conjunctions[sn2];
+        }
+        my_comment += "。";
+        items.eq(i).find('textarea').text(my_comment);
+        console.log("第" + i + "个商品评价完成");
+    }
 }
+
